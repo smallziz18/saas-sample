4164c3b39a63c1b002f77a347031bd71df30231d
smallziz18 <abdoulaziz17.diouf@ucad.edu.sn>
2024-06-18 09:53:07 +0000

    preparation des fichier statics

--- src/smallzizhome/settings.py
+++ src/smallzizhome/settings.py
@@ -144,7 +144,20 @@ USE_TZ = True
 # Static files (CSS, JavaScript, Images)
 # https://docs.djangoproject.com/en/5.0/howto/static-files/
 
-STATIC_URL = 'static/'
+STATIC_URL = '/static/'
+STATICFILES_BASE_DIR = BASE_DIR / "staticfiles"
+STATICFILES_VENDOR_DIR = STATICFILES_BASE_DIR / "vendors"
+
+STATICFILES_DIRS = [
+    STATICFILES_BASE_DIR,
+]
+
+STATIC_ROOT = BASE_DIR / "local-cdn"
+
+STATIC_ROOT = BASE_DIR / "local-cdn"
+
+
+
 
 # Default primary key field type
 # https://docs.djangoproject.com/en/5.0/ref/settings/#default-auto-field
