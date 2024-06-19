cbeb2c99bdd64272391b414b4ed9096a91b47b8b
smallziz18 <abdoulaziz17.diouf@ucad.edu.sn>
2024-06-19 09:41:59 +0000

    telechargement automaque des fichier statics

--- src/smallzizhome/settings.py
+++ src/smallzizhome/settings.py
@@ -28,7 +28,6 @@ SECRET_KEY = config("DJANGO_SECRET_KEY")
 # DEBUG = str(os.environ.get("DJANGO_DEBUG")).lower =="true"
 DEBUG = config("DJANGO_DEBUG", cast=bool)
 
-print("DEBUG",DEBUG,type(DEBUG))
 
 ALLOWED_HOSTS = [
     ".railway.app"
@@ -96,7 +95,7 @@ DATABASES = {
         'NAME': BASE_DIR / 'db.sqlite3',
     }
 }
-DATABASE_URL = config("DATABASE_URL",cast=str)
+DATABASE_URL = config("DATABASE_URL",default=None)
 CONN_MAX_AGE = config("CONN_MAX_AGE",cast=int,default=30)
 
 if DATABASE_URL is not None:
